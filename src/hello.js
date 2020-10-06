7f446d499d3cce2041df179dfa8da0451d85f6c9
Ken Uehara <kenuehara@KennoMacBook-Pro.local>
2020-10-06 21:56:08 +0900

    studied props

---
+++ src/hello.js
@@ -0,0 +1,31 @@
+import React,{ Component } from 'react';
+
+/* This class extends Component in react package using class.
+class Hello extends Component{
+    render(){
+        return <div>Hello World!!</div>;
+    }
+}
+
+export default Hello;
+
+*/
+
+/* function based
+
+import React from 'react';
+
+const Hello = () =>{
+    return <p>Hello World!!</p>;
+};
+
+export default Hello;
+
+*/
+
+const Hello = (props) =>{
+    return <p>Hello My name is (props.name).</p>;
+};
+
+// <p>Hello, my name is Taro.</p>
+<Hello name = "Taro" />
